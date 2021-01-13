49d49d74c904d7bf5ef0abb4887e59b82b668c15
Sauharda Rajbhandari <sauharda.rajbhandari@appfolio.com>
2021-01-13 18:37:46 -0500

    SR - Add create image action and strong params for image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,15 @@ class ImagesController < ActionController::Base
   def index
     @image = Image.new
   end
+
+  def create
+    img = Image.create!(image_params)
+    redirect_to image_path(img.id)
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:link)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -6,4 +6,10 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'create an image' do
+    assert_difference 'Image.count' do
+      post images_path, params: { image: { link: 'https://www.appfolio.com/images/html/apm-fb-logo.png' } }
+      assert_redirected_to image_path(Image.last)
+    end
+  end
 end
