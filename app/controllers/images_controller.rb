0b35079c6f35dd2d8bfc1d7771a5a97390c4bfff
Sauharda Rajbhandari <sauharda.rajbhandari@appfolio.com>
2021-01-14 12:17:41 -0500

    SR - Remove images index page and controller action, and remove index test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,4 @@
 class ImagesController < ActionController::Base
-  def index
-    @image = Image.new
-  end
-
   def create
     img = Image.create!(image_params)
     redirect_to image_path(img.id)
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get images_path
-    assert_response :success
-  end
-
   test 'create an image' do
     assert_difference 'Image.count' do
       post images_path, params: { image: { link: 'https://www.appfolio.com/images/html/apm-fb-logo.png' } }
