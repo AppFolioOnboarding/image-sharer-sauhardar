e12c69a4f11282ab8938f4b94edf22aa0769aeff
Sauharda Rajbhandari <sauharda.rajbhandari@appfolio.com>
2021-01-19 18:37:25 -0500

    SR - add top-level App component and tests

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
 import Header from './Header';
+import FeedbackForm from './FeedbackForm';
+import Footer from './Footer';
 
 export default function App() {
   return (
-    <div>
-      <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
-    </div>
+    <Container>
+      <Row >
+        <Col md={{ span: 4, offset: 4 }} >
+          <Header title="Tell us what you think" />
+        </Col>
+      </Row>
+      <Row>
+        <FeedbackForm />
+      </Row>
+      <Row>
+        <Col md={{ span: 3, offset: 5 }}>
+          <Footer text="Copyright: AppFolio Inc. Onboarding" />
+        </Col>
+      </Row>
+    </Container>
+
   );
 }
 
