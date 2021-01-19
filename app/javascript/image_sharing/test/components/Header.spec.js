c6501f9bb4c37ff669b97602fe36e2baa4396390
Sauharda Rajbhandari <sauharda.rajbhandari@appfolio.com>
2021-01-19 18:37:14 -0500

    SR - add Header and tests

--- app/javascript/image_sharing/test/components/Header.spec.js
+++ app/javascript/image_sharing/test/components/Header.spec.js
@@ -1,13 +1,13 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Header from '../../components/Header';
 
 describe('<Header />', () => {
   it('should render correctly', () => {
-    const wrapper = mount(<Header title="Title" />);
+    const wrapper = shallow(<Header title="Title" />);
     const title = wrapper.find('header');
 
     assert.strictEqual(title.length, 1);
